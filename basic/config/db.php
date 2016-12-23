a2cd95b7dc0d05a23f25cffd24ecdd8fe987c6cc
u4bi <u4bi@u4bi.com>
2016-12-23 17:14:14 +0000

    api/customers db con, select render

--- basic/config/db.php
+++ basic/config/db.php
@@ -8,7 +8,7 @@
     /* 연결*/
     public function connect(){
       $mysql_connect_str ="mysql:host=$this->db_host; dbname=$this->db_name";
-      $dbConnection = new PDO(mysql_connect_str, $this->db_user, $this->db_pass);
+      $dbConnection = new PDO($mysql_connect_str, $this->db_user, $this->db_pass);
       /* http://php.net/manual/kr/pdo.connections.php */
       
       $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- basic/routes/customers.php
+++ basic/routes/customers.php
@@ -11,7 +11,21 @@ $app = new \Slim\App;
 
 $app->get('/api/customers',
   function (Request $request, Response $response) {
-  echo 'get api customers';
+  $sql = "SELECT * FROM customers"; /* 쿼리문*/
+  
+  try{
+    $db = new db(); /* db 클래스 선언 */
+    $db = $db -> connect(); /* db 연결 */
+    
+    $stmt = $db->query($sql); /* 쿼리실행*/
+    $customers = $stmt->fetchAll(PDO::FETCH_OBJ); /*fetchAll 배열로 돌림*/
+    /* http://php.net/manual/kr/pdostatement.fetchall.php */
+    $db = null; /* null 정의*/
+    
+    echo json_encode($customers); /* 배열로 담긴 객체를 json으로 인코딩하여 출력*/
+    
+  } catch(PDOException $e){ /* 인셉션처리*/
+    echo '{"error": {"text": '.$e->getMessage().'}'; /* 에러 json을 보넴*/}
 });
 
 $app->run();
