b92fff5b31e43b7a6ee5c378e2835aa9fde954b1
u4bi <u4bi@u4bi.com>
2016-12-23 16:19:42 +0000

    db conn init

---
+++ basic/config/db.php
@@ -0,0 +1,19 @@
+<?php
+  class db{
+    private $db_host = 'localhost';
+    private $db_user = 'root';
+    private $db_pass = '';
+    private $db_name = 'u4bi';
+    
+    /* 연결*/
+    public function connect(){
+      $mysql_connect_str ="mysql:host=$this->db_host; dbname=$this->db_name";
+      $dbConnection = new PDO(mysql_connect_str, $this->db_user, $this->db_pass);
+      /* http://php.net/manual/kr/pdo.connections.php */
+      
+      $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      /* http://php.net/manual/kr/pdo.setattribute.php */
+      return $dbConnection;
+    }
+  }
+?>
