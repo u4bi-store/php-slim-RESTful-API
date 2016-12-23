2e31f3ea8491b7c3312aaef16129459c98be94ce
u4bi <u4bi@u4bi.com>
2016-12-23 17:24:48 +0000

    get single customers render

--- basic/routes/customers.php
+++ basic/routes/customers.php
@@ -7,8 +7,7 @@ require '../config/db.php';
 
 $app = new \Slim\App;
 
-/* get customers */
-
+/* get all customers */
 $app->get('/api/customers',
   function (Request $request, Response $response) {
   $sql = "SELECT * FROM customers"; /* 쿼리문*/
@@ -23,6 +22,29 @@ $app->get('/api/customers',
     $db = null; /* null 정의*/
     
     echo json_encode($customers); /* 배열로 담긴 객체를 json으로 인코딩하여 출력*/
+    
+  } catch(PDOException $e){ /* 인셉션처리*/
+    echo '{"error": {"text": '.$e->getMessage().'}'; /* 에러 json을 보냄*/
+  }
+});
+
+/*get single customers*/
+$app->get('/api/customers/{id}',
+  function (Request $request, Response $response) {
+  $id = $request->getAtrribute('id');
+  
+  $sql = "SELECT * FROM customers WHERE id = $id"; /* 쿼리문*/
+  
+  try{
+    $db = new db(); /* db 클래스 선언 */
+    $db = $db -> connect(); /* db 연결 */
+    
+    $stmt = $db->query($sql); /* 쿼리실행*/
+    $customer = $stmt->fetchAll(PDO::FETCH_OBJ); /*fetchAll 배열로 돌림*/
+    /* http://php.net/manual/kr/pdostatement.fetchall.php */
+    $db = null; /* null 정의*/
+    
+    echo json_encode($customer); /* 배열로 담긴 객체를 json으로 인코딩하여 출력*/
     
   } catch(PDOException $e){ /* 인셉션처리*/
     echo '{"error": {"text": '.$e->getMessage().'}'; /* 에러 json을 보냄*/
