52b7bdd5241f53717f811f1756d1d3687e2f621f
u4bi <u4bi@u4bi.com>
2017-02-06 00:25:47 +0900

    user info insert api render

--- game-api/routes/user/user-info.php
+++ game-api/routes/user/user-info.php
@@ -45,3 +45,52 @@ $app -> get('/api/user-info/{id}', function(Request $req, Response $res){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
 });
+
+$app -> post('/api/user-info/add', function(Request $req, Response $res){
+    
+    $name = $req -> getParam('name');
+    $age = $req -> getParam('age');
+    $level = $req -> getParam('level');
+    $vio = $req -> getParam('vio');
+    $team = $req -> getParam('team');
+    $skill = $req -> getParam('skill');
+    $nation = $req -> getParam('nation');
+
+    $sql = "INSERT INTO user_info(
+                name,
+                age,
+                level,
+                vio,
+                team,
+                skill,
+                nation)
+            VALUES(
+                :name,
+                :age,
+                :level,
+                :vio,
+                :team,
+                :skill,
+                :nation)";
+    try{
+        $db = new db();
+        $db = $db->connect();
+
+        $stmt = $db-> prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':age', $age);
+        $stmt->bindParam(':level', $level);
+        $stmt->bindParam(':vio', $vio);
+        $stmt->bindParam(':team', $team);
+        $stmt->bindParam(':skill', $skill);
+        $stmt->bindParam(':nation', $nation);
+        
+        $stmt->execute();
+
+        echo '{"notice": {"text": "user-info add"}';
+
+    } catch(PDOEception $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
