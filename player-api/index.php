ba6f11ca29c3fe9f68a972c49da3e2a012fcbb46
u4bi <u4bi@u4bi.com>
2017-02-21 17:13:01 +0900

    player api db con init

---
+++ player-api/index.php
@@ -0,0 +1,11 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require 'database/dbcon.php';
+
+$app = new \Slim\App;
+
+
+$app->run();
